8589dba69d7ccab9ddae4299e2acd7a9085781e9
FilipCon <filipconstantinos@gmail.com>
2020-06-01 19:33:57 +0300

    add exec-path package

---
+++ config/shell.el
@@ -0,0 +1,11 @@
+;;; -*- lexical-binding: t -*-
+
+(if (not (eq system-type 'windows-nt))
+    (use-package exec-path-from-shell
+      :init
+      (when (display-graphic-p)
+        (exec-path-from-shell-copy-env "LD_LIBRARY_PATH")
+        (exec-path-from-shell-initialize))
+      :config
+      ;;   (setq exec-path-from-shell-arguments '("-l"))
+      (setq exec-path-from-shell-check-startup-files nil)))
