4ab841c4b662f9175f6b0b56a411e5750e0a5435
FilipCon <filipconstantinos@gmail.com>
2020-11-28 23:04:39 +0200

    cider v0.24 for arcadia, csharp mode

---
+++ config/csharp.el
@@ -0,0 +1,5 @@
+;;; -*- lexical-binding: t -*-
+
+(use-package csharp-mode)
+
+(use-package omnisharp)
--- config/lisp.el
+++ config/lisp.el
@@ -1,7 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
-;; elisp
-(use-package package-lint)
+(use-package cider
+  :straight (:host github :repo "clojure-emacs/cider" :branch "v0.24.0")
+  :bind (:map cider-mode-map
+              ("C-c C-b" . cider-eval-buffer)
+              ("C-c C-c" . cider-eval-defun-at-point))
+  :config
+  (global-unset-key (kbd "C-c C-f"))
+  (setq cider-completion-annotations-include-ns 'always)
+  (add-to-list 'cider-test-defining-forms "deftest-db"))
 
 ;; clojure TODO:
 (use-package clojure-mode)
@@ -12,11 +19,5 @@
 (use-package clj-refactor
   :hook (clojure-mode . clj-refactor-mode))
 
-(use-package cider
-  :bind (:map cider-mode-map
-              ("C-c C-b" . cider-eval-buffer)
-              ("C-c C-c" . cider-eval-defun-at-point))
-  :config
-  (global-unset-key (kbd "C-c C-f"))
-  (setq cider-completion-annotations-include-ns 'always)
-  (add-to-list 'cider-test-defining-forms "deftest-db"))
+;; elisp
+(use-package package-lint)
